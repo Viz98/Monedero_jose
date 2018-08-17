d618e5a9fcd07168d4aaedf7fb89da00e6d52b42
Viz98 <42260584+Viz98@users.noreply.github.com>
2018-08-17 15:33:35 -0600

    sadasd

--- Monedero_Electronico/src/Monedero/Monedero.java
+++ Monedero_Electronico/src/Monedero/Monedero.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Monedero;
+//package Monedero;
 
 import Controlador.ControlLogin;
 import Controlador.ControladorMonedero;
